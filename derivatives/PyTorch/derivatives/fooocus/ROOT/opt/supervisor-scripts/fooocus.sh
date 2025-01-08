47a1f52aa80c76b36219b84c60f75a6d4ac0945c
Rob Ballantyne <rob@robballantyne.co.uk>
2025-01-08 14:59:32 +0000

    Add Fooocus

---
+++ derivatives/PyTorch/derivatives/fooocus/ROOT/opt/supervisor-scripts/fooocus.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# User can configure startup by removing the reference in /etc.portal.yaml - So wait for that file and check it
+while [ ! -f "$(realpath -q /etc/portal.yaml 2>/dev/null)" ]; do
+    echo "Waiting for /etc/portal.yaml before starting ${PROC_NAME}..." | tee -a "/var/log/portal/${PROC_NAME}.log"
+    sleep 1
+done
+
+# rudimentary check for fooocus in the portal config
+search_pattern=$(echo "$PROC_NAME" | sed 's/[ _-]/[ _-]/g')
+if ! grep -qiE "^[^#].*${search_pattern}" /etc/portal.yaml; then
+    echo "Skipping startup for ${PROC_NAME} (not in /etc/portal.yaml)" | tee -a "/var/log/portal/${PROC_NAME}.log"
+    exit 0
+fi
+
+# Activate the venv
+. ${DATA_DIRECTORY}venv/main/bin/activate
+
+# Wait for provisioning to complete
+
+while [ -f "/.provisioning" ]; do
+    echo "$PROC_NAME startup paused until instance provisioning has completed (/.provisioning present)"
+    sleep 10
+done
+
+# Launch Forge
+cd ${DATA_DIRECTORY}Fooocus
+LD_PRELOAD=libtcmalloc_minimal.so.4 \
+        python launch.py \
+        ${FOOOCUS_ARGS:---port 17865} | tee -a "/var/log/portal/${PROC_NAME}.log"
