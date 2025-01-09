7c9f34f6203f3291545eaf53aa289b2da687b4b2
Rob Ballantyne <rob@robballantyne.co.uk>
2025-01-09 17:06:29 +0000

    Add SwarmUI

---
+++ derivatives/PyTorch/derivatives/swarmui/ROOT/opt/supervisor-scripts/swarmui.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# User can configure startup by removing the reference in /etc.portal.yaml - So wait for that file and check it
+while [ ! -f "$(realpath -q /etc/portal.yaml 2>/dev/null)" ]; do
+    echo "Waiting for /etc/portal.yaml before starting ${PROC_NAME}..." | tee -a "/var/log/portal/${PROC_NAME}.log"
+    sleep 1
+done
+
+# rudimentary check for fooocus in the portal config
+search_pattern=$(echo "$PROC_NAME" | sed 's/[ _-]/[ _-]/g')
+if ! grep -qiE "^[^#].*${search_pattern}" /etc/portal.yaml; then
+    echo "Skipping startup for ${PROC_NAME} (not in /etc/portal.yaml)" | tee -a "/var/log/portal/${PROC_NAME}.log"
+    exit 0
+fi
+
+# Activate the venv
+. ${DATA_DIRECTORY}venv/main/bin/activate
+
+# Wait for provisioning to complete
+
+while [ -f "/.provisioning" ]; do
+    echo "$PROC_NAME startup paused until instance provisioning has completed (/.provisioning present)"
+    sleep 10
+done
+
+# Launch SwarmUI
+cd ${DATA_DIRECTORY}SwarmUI
+     \
+        ./launch-linux.sh ${SWARMUI_ARGS:---launch_mode none --port 17865} | tee -a "/var/log/portal/${PROC_NAME}.log"
