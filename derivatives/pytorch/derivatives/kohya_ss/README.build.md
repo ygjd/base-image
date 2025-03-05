5da7ce607eb26e5e1df51b09ccc5af21df42307c
Rob Ballantyne <rob@robballantyne.co.uk>
2025-03-05 14:54:54 +0000

    Add kohya_ss image

---
+++ derivatives/pytorch/derivatives/kohya_ss/README.build.md
@@ -0,0 +1,13 @@
+# Building
+
+This Dockerfile relies on the structure provided by [Vast.Ai Base images](https://github.com/vast-ai/base-image).
+
+See the example below for building instructions.
+
+```bash
+docker buildx build \
+    --platform linux/amd64 \
+    --build-arg PYTORCH_BASE=vastai/pytorch:2.5.1-cuda-12.1.1 \
+    --build-arg KOHYA_REF=78c79e0 \
+    . -t repo/image:tag --push
+```
---
+++ derivatives/pytorch/derivatives/kohya_ss/ROOT/opt/supervisor-scripts/kohya.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# User can configure startup by removing the reference in /etc.portal.yaml - So wait for that file and check it
+while [ ! -f "$(realpath -q /etc/portal.yaml 2>/dev/null)" ]; do
+    echo "Waiting for /etc/portal.yaml before starting ${PROC_NAME}..." | tee -a "/var/log/portal/${PROC_NAME}.log"
+    sleep 1
+done
+
+# Check for kohya in the portal config
+search_term="kohya"
+search_pattern=$(echo "$search_term" | sed 's/[ _-]/[ _-]/g')
+if ! grep -qiE "^[^#].*${search_pattern}" /etc/portal.yaml; then
+    echo "Skipping startup for ${PROC_NAME} (not in /etc/portal.yaml)" | tee -a "/var/log/portal/${PROC_NAME}.log"
+    exit 0
+fi
+
+# Activate the venv
+. /venv/main/bin/activate
+
+# Wait for provisioning to complete
+
+while [ -f "/.provisioning" ]; do
+    echo "$PROC_NAME startup paused until instance provisioning has completed (/.provisioning present)" | tee -a "/var/log/portal/${PROC_NAME}.log"
+    sleep 10
+done
+
+# Avoid git errors because we run as root but files are owned by 'user'
+export GIT_CONFIG_GLOBAL=/tmp/temporary-git-config
+git config --file $GIT_CONFIG_GLOBAL --add safe.directory '*'
+
+# Launch Kohya's GUI
+cd ${WORKSPACE}/kohya_ss
+LD_PRELOAD=libtcmalloc_minimal.so.4 \
+        python kohya_gui.py \
+        ${KOHYA_ARGS:---server_port 17860 --headless --noverify} 2>&1 | tee -a "/var/log/portal/${PROC_NAME}.log"
